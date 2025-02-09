ad18256fb37fe11dfdfc9bb9b4f6d9bfad6d52ad
radinkaputri <154575904+radinkaputri@users.noreply.github.com>
2025-02-09 07:44:29 +0700

    typo

--- bot/helper/mirror_utils/status_utils/telegram_status.py
+++ bot/helper/mirror_utils/status_utils/telegram_status.py
@@ -5,7 +5,7 @@ from bot.helper.ext_utils.bot_utils import (MirrorStatus,
                                             get_readable_file_size,
                                             get_readable_time)
 
-engine_ = f"pyrogram v{get_distribution('pyrogran').version}"
+engine_ = f"pyrogram v{get_distribution('pyrogram').version}"
 
 
 class TelegramStatus:
